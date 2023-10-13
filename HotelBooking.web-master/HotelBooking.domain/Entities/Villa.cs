b3d3e9758d23113198a96977bf86ed0bee9f17f9
Chaitanya Sharma <chait8126@gmail.com>
2023-10-13 09:45:17 +0530

    cloning

---
+++ HotelBooking.web-master/HotelBooking.domain/Entities/Villa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBooking.Domain.Entities
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        [DisplayName("Villa Name")]
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+
+        [DisplayName("Price per night")]
+        public double Price { get; set; }
+
+        public int Sqft { get; set; }
+
+        public int Occupancy { get; set; }
+
+        [DisplayName("Image Url")]
+        public string? ImageUrl { get; set; }
+
+        public DateTime? Created_Date { get; set; }
+        public DateTime? Updated_Date { get;set; }
+
+    }
+}
---
+++ HotelBooking.web-master/HotelBooking.web/Controllers/VillaController.cs
@@ -0,0 +1,34 @@
+﻿using HotelBooking.Domain.Entities;
+using HotelBooking.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBooking.Web.Controllers
+{
+    
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaController(ApplicationDbContext context)
+        {
+            _db = context;
+        }
+        public IActionResult Index()
+        {
+            var villa = _db.Villas.ToList();
+            return View(villa);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            _db.Villas.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
