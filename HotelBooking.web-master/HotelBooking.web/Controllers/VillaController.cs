aa78f19f63d05383a4bebf3ff63cca6c89caa05c
Chaitanya Sharma <chait8126@gmail.com>
2023-10-17 00:43:05 +0530

    villa crud

--- HotelBooking.web-master/HotelBooking.web/Controllers/VillaController.cs
+++ HotelBooking.web-master/HotelBooking.web/Controllers/VillaController.cs
@@ -1,6 +1,7 @@
 ﻿using HotelBooking.Domain.Entities;
 using HotelBooking.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HotelBooking.Web.Controllers
 {
@@ -54,11 +55,37 @@ namespace HotelBooking.Web.Controllers
             return View(villa);
         }
 
-        [HttpPost]
-        public IActionResult Update(Villa villa)
+		[HttpPost]
+		public IActionResult Update(Villa obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Villas.Update(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index", "Villa");
+			}
+			else
+			{
+				return View(obj);
+			}
+		}
+        [HttpGet]
+        public IActionResult Delete(int villaId)
         {
-            return RedirectToAction("Index", "Villa");
+            Villa? villa=_db.Villas.FirstOrDefault(x=>x.Id== villaId);
+            if(villa!=null)
+                return View(villa);
+            return RedirectToAction("Error", "Home");
         }
-
+        [HttpPost]
+        public IActionResult Delete(Villa villa)
+        {
+            var AlteredRows=_db.Villas.Where(x=>x.Id== villa.Id).ExecuteDelete();
+            if (AlteredRows == 0)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return RedirectToAction("Index");
+        }
 	}
 }
